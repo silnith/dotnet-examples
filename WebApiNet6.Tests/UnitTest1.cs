08a32f263f7892459c13515ff3b864c5147a577b
silnith <silnith@gmail.com>
2023-01-17 05:28:59 -0600

    Add trivial unit test.

--- WebApiNet6.Tests/UnitTest1.cs
+++ WebApiNet6.Tests/UnitTest1.cs
@@ -1,11 +1,18 @@
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+using WebApiNet6.Controllers;
+
 namespace WebApiNet6.Tests
 {
     [TestClass]
     public class UnitTest1
     {
+        private readonly ILogger<WeatherForecastController> logger = new NullLogger<WeatherForecastController>();
+
         [TestMethod]
         public void TestMethod1()
         {
+            _ = new WeatherForecastController(logger);
         }
     }
 }
