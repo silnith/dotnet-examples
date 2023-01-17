2e0038aabc52dbad5f9a6755fa8a7d6b8a3a7060
silnith <silnith@gmail.com>
2023-01-17 05:33:49 -0600

    Add trivial integration test.

--- WebApiNet6.IntegrationTests/UnitTest1.cs
+++ WebApiNet6.IntegrationTests/UnitTest1.cs
@@ -1,11 +1,23 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using System.Net;
+
 namespace WebApiNet6.IntegrationTests
 {
     [TestClass]
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public async Task TestMethod1()
         {
+            using WebApplicationFactory<Program> factory = new();
+
+            using var client = factory.CreateDefaultClient();
+
+            using HttpRequestMessage httpRequestMessage = new(HttpMethod.Get, new Uri("/swagger/index.html", UriKind.Relative));
+
+            using var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+
+            Assert.AreEqual(HttpStatusCode.OK, httpResponseMessage.StatusCode);
         }
     }
 }
